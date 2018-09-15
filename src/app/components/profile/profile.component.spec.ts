498b901ef6a9887ccfaf3b59d328a0b704e3cce2
Yesenia <eliyessie@gmail.com>
2018-09-14 19:23:43 -0700

    removed unecessary import

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 
