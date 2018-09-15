584f1ea30d2445c6272572f44662e079ed5553dd
Yesenia <eliyessie@gmail.com>
2018-09-14 19:39:48 -0700

    audited some vulnerabilities

---
+++ src/app/components/nav/nav.component.spec.ts
@@ -0,0 +1,31 @@
+
+import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NavComponent } from './nav.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('NavComponent', () => {
+  let component: NavComponent;
+  let fixture: ComponentFixture<NavComponent>;
+
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ MatSidenavModule, HttpClientModule, RouterTestingModule, BrowserAnimationsModule ],
+      // added router testing module. and the http client module
+      declarations: [ NavComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ] //
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
